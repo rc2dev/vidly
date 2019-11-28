5b5df8ed8d10486eae09139d055df4381cbef9f3
Rafael Cavalcanti <rccavalcanti@gmail.com>
2019-11-28 16:23:55 -0300

    Extract table to own component

---
+++ src/components/common/table.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import TableHeader from './tableHeader';
+import TableBody from './tableBody';
+
+const Table = props => {
+  const { columns, sortColumn, onSort, data } = props;
+
+  return (
+    <table className="table">
+      <TableHeader columns={columns} onSort={onSort} sortColumn={sortColumn} />
+      <TableBody data={data} columns={columns} />
+    </table>
+  );
+};
+
+export default Table;
