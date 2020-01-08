0465e23a8001dc11d9a8bffd70bd4c2b02cee70c
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-01-08 14:47:46 -0300

    Initial implementation of login

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Form from './common/form';
 import Joi from 'joi-browser';
+import { login } from '../services/authService';
 
 class LoginForm extends Form {
   state = { data: { username: '', password: '' }, errors: {} };
@@ -15,8 +16,19 @@ class LoginForm extends Form {
       .label('Password')
   };
 
-  doSubmit() {
-    console.log('Submitted');
+  async doSubmit() {
+    try {
+      const { data } = this.state;
+      const { data: jwt } = await login(data.username, data.password);
+      localStorage.setItem('token', jwt);
+      this.props.history.push('/');
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   }
 
   render() {
