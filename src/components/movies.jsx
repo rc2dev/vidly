0d23ed6e404b9e21c2cfba2e9bbe9daca733c9a0
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-01-14 11:44:56 -0300

    Render tables even if no movies

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -108,8 +108,6 @@ class Movies extends Component {
     } = this.state;
     const { user } = this.props;
 
-    if (count === 0) return <p>There are no movies in the database.</p>;
-
     const { totalCount, data: movies } = this.getPagedData();
 
     return (
